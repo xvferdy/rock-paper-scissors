77d56c590c8238d8e2edbb922969287ead9595d8
ferdy <xv.ferdy@gmail.com>
2019-03-29 16:53:01 +0700

    Add javascript file

---
+++ script.js
@@ -0,0 +1,36 @@
+//function global yang akan dipanggil di dlm function untuk dijadikan variabel
+//function pilihan komputer
+//function mengembalikan sebuah nilai (disimpan)
+function getpilihankomputer() {
+    var comp = Math.random();
+    if (comp < 0.34) return 'gunting';
+    if (comp >= 0.34 && comp < 0.67) return 'batu';
+    return 'kertas';
+}
+
+//function global yang akan dipanggil di dlm function untuk dijadikan variabel
+//function aturan
+//menggunakan operator conditional
+//function mengembalikan sebuah nilai (disimpan)
+function getHasil(comp, player) {
+    if (player == comp) return 'SERI!';
+    if (player == 'gunting') return (comp == 'kertas') ? 'MENANG!' : 'KALAH!';
+    if (player == 'batu') return (comp == 'kertas') ? 'KALAH!' : 'MENANG!';
+    if (player == 'kertas') return (comp == 'gunting') ? 'KALAH!' : 'MENANG!';
+}
+
+//event
+//menangkap element
+var imgComputer = document.querySelector('.img-komputer');
+var info = document.querySelector('.info');
+var pilihan = document.querySelectorAll('li img');
+//jalankan event
+pilihan.forEach(function (i) {
+    i.addEventListener('click', function () {
+        var pilihanKomputer = getpilihankomputer()
+        var pilihanPlayer = i.className;
+        var hasil = getHasil(pilihanKomputer, pilihanPlayer);
+        imgComputer.setAttribute('src', 'images/' + pilihanKomputer + '.png');
+        info.innerHTML = hasil;
+    })
+})
